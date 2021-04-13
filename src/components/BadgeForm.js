a319907a7a7fcf79bb08689f1daedf886ac43943
OsvaldoGalvan <osvaldo@osvaldogalvan.com>
2021-04-13 15:53:02 -0500

    Levantamiento de estado

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -2,23 +2,13 @@ import React from 'react'
 
 class BadgeForm extends React.Component {
 
-    state = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        jobTitle: 'Designer',
-        twitter: ''
-      };
+    state = {};
       
-    handleChange = (e) => {
-        // console.log({
-        //     name: e.target.name,
-        //     value: e.target.value
-        // })
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+    // handleChange = (e) => {
+    //     this.setState({
+    //         [e.target.name]: e.target.value
+    //     })
+    // }
 
     handleClick = (e) => {
         // console.log("Presiono botón")
@@ -40,47 +30,47 @@ class BadgeForm extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label>First Name</label>
-                        <input onChange={this.handleChange} 
+                        <input onChange={this.props.onChange} 
                             className="form-control" 
                             type="text" 
                             name="firstName"
-                            value={this.state.firstName}></input>
+                            value={this.props.formValues.firstName}></input>
                     </div>
 
                     <div className="form-group">
                         <label>Last  Name</label>
-                        <input onChange={this.handleChange} 
+                        <input onChange={this.props.onChange} 
                             className="form-control" 
                             type="text" 
                             name="lastName"
-                            value={this.state.lastName}></input>
+                            value={this.props.formValues.lastName}></input>
                     </div>
 
                     <div className="form-group">
                         <label>Email</label>
-                        <input onChange={this.handleChange} 
+                        <input onChange={this.props.onChange} 
                             className="form-control" 
                             type="email" 
                             name="Email"
-                            value={this.state.Email}></input>
+                            value={this.props.formValues.Email}></input>
                     </div>
 
                     <div className="form-group">
                         <label>Job Title</label>
-                        <input onChange={this.handleChange} 
+                        <input onChange={this.props.onChange} 
                             className="form-control" 
                             type="text" 
                             name="jobTitle"
-                            value={this.state.jobTitle}></input>
+                            value={this.props.formValues.jobTitle}></input>
                     </div>
 
                     <div className="form-group">
                         <label>Twitter</label>
-                        <input onChange={this.handleChange} 
+                        <input onChange={this.props.onChange} 
                             className="form-control" 
                             type="text" 
                             name="twitter"
-                            value={this.state.twitter}></input>
+                            value={this.props.formValues.twitter}></input>
                     </div>
 
                     <button onClick={this.handleClick} className="btn btn-primary">Save</button>
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,43 @@ import Navbar from '../components/Navbar.js'
 import BadgeForm from '../components/BadgeForm.js'
 
 class BadgeNew extends React.Component {
+    state = {
+        form : {
+            firstName :'',
+            lastName : '',
+            email : '',
+            jobTitle : '',
+            twitter : ''
+        } // Creamos un estado que sera un objeto
+    }
+        
+    handleChange = e => {
+        // 1.- Paso
+        // this.setState({
+        //     form : {
+        //         [e.target.name]: e.target.value
+        //     }
+        // })
+
+        // 2.- Paso
+        // const nextForm = this.state.form
+        // nextForm[e.target.name] = e.target.value
+        
+        // this.setState({
+        //     form : nextForm
+        // })
+
+        // 3.- Paso
+        this.setState({
+             form : {
+                 ...this.state.form,
+                 [e.target.name]: e.target.value,
+             }
+         })        
+
+    }
+
+
     render(){
         return (
             <div>
@@ -19,16 +56,19 @@ class BadgeNew extends React.Component {
                     <div className="row">
                         <div className="col-6">
                             <Badge
-                                firstName="Osvaldo"
-                                lastName="Galván"
-                                jobTitle="Softwware Enginner" 
-                                twitter = "@OsvaldoGalvan"
+                                firstName= {this.state.form.firstName}
+                                lastName = {this.state.form.lastName}
+                                jobTitle = {this.state.form.jobTitle}
+                                twitter  = {this.state.form.twitter}
+                                email    = {this.state.form.email}
                                 avatarURL ="https://s.gravatar.com/avatar/2576a730e8561d306104b2806ed8105e?s=80"
                                 />
                         </div>
 
                         <div className="col-6">
-                            <BadgeForm />
+                            <BadgeForm 
+                                onChange={this.handleChange} 
+                                formValues={this.state.form}/>
                         </div>
                     </div>
                 </div>
