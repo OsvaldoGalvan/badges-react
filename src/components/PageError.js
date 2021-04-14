f9d25bbc69e78a2b39f10e0dc19b32706b6cb5cb
OsvaldoGalvan <osvaldo@osvaldogalvan.com>
2021-04-14 13:06:29 -0500

    Page Loader y Page Error

---
+++ src/components/PageError.js
@@ -0,0 +1,9 @@
+import './styles/PageError.css'
+
+function PageError(props){
+    return (
+        <div className="PageError">{props.error.message}</div>
+    )
+}
+
+export default PageError
---
+++ src/components/PageLoading.js
@@ -0,0 +1,10 @@
+import './styles/PageLoading.css'
+import Loader from './Loader'
+
+function PageLoading (){
+    return (
+        <div className="PageLoading"><Loader /></div>
+    )
+}
+
+export default PageLoading
